a2f07861cea7a717feccc7a27256d72e23f5816e
ahmedfathy21 <150164291+ahmedfathy21@users.noreply.github.com>
2023-12-12 16:48:33 +0200

    Update Task7_Q6.c

--- task_7/Task7_Q6.c
+++ task_7/Task7_Q6.c
@@ -4,15 +4,9 @@
 int main()
 {
    char sentence[100];
-
-  // Get the sentence from the user
   printf("Enter a sentence: ");
   fgets(sentence, 100, stdin);
-
-  // Remove the trailing newline character
   sentence[strlen(sentence) - 1] = '\0';
-
-  // Extract each word and print it on a new line
   char *word = strtok(sentence, " ");
   while (word != NULL) {
     printf("%s\n", word);
